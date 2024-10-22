6cc0049e8363cdd06da499d797cdc2dea2c48afa
Darrell Hubbard <46301396+darrellhubbardportfolio@users.noreply.github.com>
2024-10-22 15:21:25 -0400

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,9 +18,13 @@ fetch("/api/toggle")
 class Toggle {
 
     constructor (model, key, value, id) {
-        this.container = document.querySelector("main"); // container where the toggle-switch will reside
+        this.container = document.querySelector("#toggle-centerfold"); // container where the toggle-switch will reside
         this.toggle = document.createElement("div");
         this.toggle_switch = document.createElement("button"); // the switch
+        // create the label
+        this.switch_label = document.createElement("div");
+        this.sun_label = document.createElement("i");
+        this.moon_label = document.createElement("i");
         this.model = model;
         this.key = key;
         this.value = value;
@@ -33,17 +37,25 @@ class Toggle {
         });
         // add properties
         this.toggle.className = "toggle";
+        // add class name to toggle depending upon what the value is from this.data.
+        (this.data.value === "false")? this.toggle.classList.add("toggle-night-mode") : this.toggle.classList.add("toggle-day-mode");
         this.toggle_switch.className = "toggle-switch";
         this.toggle_switch.dataset.model = this.data.model;
         this.toggle_switch.dataset.key = this.key;
         this.toggle_switch.dataset.value = this.value;
         this.toggle_switch.dataset.id = this.id;
+        this.switch_label.className = "container d-flex align-items-center justify-content-between h-100 col-12 border-0";
+        this.sun_label.className = "bi bi-brightness-high col-6";
+        this.moon_label.className = "bi bi-moon col-6";
         // add functions to switch
         this.toggle_switch.addEventListener("click", () => {
             console.log("clicked button");
             this.updateToggle();
         });
         // add each element in correct place
+        this.switch_label.append(this.sun_label);
+        this.switch_label.append(this.moon_label);
+        this.toggle.append(this.switch_label);
         this.toggle.append(this.toggle_switch);
         this.container.append(this.toggle);
     }
@@ -86,6 +98,8 @@ class Toggle {
 
         // implement code 
         this.toggle_switch.style.animation = "toggleSlideOn 0.5s forwards";
+        this.toggle.classList.remove("toggle-night-mode");
+        this.toggle.classList.add("toggle-day-mode");
         console.log("toggle on");
     }
 
@@ -93,6 +107,8 @@ class Toggle {
     
         // implement code 
         this.toggle_switch.style.animation = "toggleSlideOff 0.5s forwards";
+        this.toggle.classList.remove("toggle-day-mode");
+        this.toggle.classList.add("toggle-night-mode");
         console.log("toggle off");
     };
 }
